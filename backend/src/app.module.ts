4c85205c0bb0bb484534366a1310d839ba984e6c
Levan Ilashvili <levan@ilashvili.me>
2026-05-03 04:23:11 +0400

    wire talk + bot + users + cron + storage modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,15 +6,23 @@ import { EthersModule } from './ethers/ethers.module';
 import { RelayModule } from './relay/relay.module';
 import { PetModule } from './pet/pet.module';
 import { StorageModule } from './storage/storage.module';
+import { TalkModule } from './talk/talk.module';
+import { BotModule } from './bot/bot.module';
+import { UsersModule } from './users/users.module';
+import { CronModule } from './cron/cron.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     EthersModule,
     AuthModule,
+    UsersModule,
+    BotModule,
     RelayModule,
     PetModule,
     StorageModule,
+    TalkModule,
+    CronModule,
   ],
   controllers: [HealthController],
 })
